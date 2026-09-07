1dadbe51b750a6ba054b1f374622ff9181fee76f
Alex <alexjm2006t@gmail.com>
2026-09-07 10:40:57 -0600

    Factorial con recursividad

---
+++ factorial.js
@@ -0,0 +1,13 @@
+function factorial(numero) {
+	if (!Number.isInteger(numero) || numero < 0) {
+		throw new Error("El numero debe ser un entero no negativo");
+	}
+
+	if (numero === 0) {
+		return 1;
+	}
+
+	return numero * factorial(numero - 1);
+}
+
+console.log(factorial(5)); // 120
