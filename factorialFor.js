d399e63db4045a7860dd956d7780ee29e3bc1903
Alex <alexjm2006t@gmail.com>
2026-09-08 11:17:42 -0600

    Nueva salida

---
+++ factorialFor.js
@@ -0,0 +1,16 @@
+function factorial(numero) {
+	if (!Number.isInteger(numero) || numero < 0) {
+		throw new Error("El numero debe ser un entero no negativo");
+	}
+
+	let resultado = 1;
+
+	for (let contador = 2; contador <= numero; contador++) {
+		resultado *= contador;
+	}
+
+	return resultado;
+}
+
+console.log(factorial(5)); // 120
+console.log(factorial(10));
